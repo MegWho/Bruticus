7f712731c49f71cfda10fb93205457d88a7063bd
Meghna Vyas <114874650+MegWho@users.noreply.github.com>
2023-09-09 09:44:45 +0530

    Add files via upload

---
+++ stars.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class stars{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float n1=sc.nextFloat();
+        float n2=sc.nextFloat();
+        float n3=n1*n2;
+        System.out.print("The product is:"+n3);
+    
+    }
+}
